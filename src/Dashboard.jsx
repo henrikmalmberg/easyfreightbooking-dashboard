bc9153f2ddf09115ac7b8679a6adb3d29c1936d2
henrikmalmberg <henrik@malmberg.cc>
2025-08-07 11:15:11 +0200

    Update Dashboard.jsx

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -486,7 +486,7 @@ setResult(null);
 {result && (
   <div className="mt-6 bg-white border rounded p-4 shadow-sm">
     <h2 className="font-semibold mb-2">Avaliable freight options</h2>
-{Object.entries(result)..filter(([_, data]) => data?.available === true) // ✅ Visa bara tillgängliga
+{Object.entries(result)..filter(([_, data]) => data && data.available === true) // ✅ Visa bara tillgängliga
   .map(([mode, data], i) => (
 <ResultCard
   key={i}
