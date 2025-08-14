729b039774b79526cb56b91f9c9e3848f59175cc
henrikmalmberg <henrik@malmberg.cc>
2025-08-14 13:59:25 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css"; // om du har tailwind
+import "./index.css";
 import ErrorBoundary from "./ErrorBoundary.jsx";
-import Dashboard from "./Dashboard.jsx"; // din toppkomponent (default export i filen)
+import App from "./Dashboard.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <Dashboard />
+      <App />
     </ErrorBoundary>
   </React.StrictMode>
 );
