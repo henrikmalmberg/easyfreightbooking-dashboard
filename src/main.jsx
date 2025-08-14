2c85a3449715105676ca7cda9dac8c94fa87bd3c
henrikmalmberg <henrik@malmberg.cc>
2025-08-14 13:07:18 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Dashboard from "./Dashboard.jsx"; // case och namn måste matcha exakt
+import "./index.css";
+
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
