cd539c2256ca99506ce78fab37af1b0566f06867
henrikmalmberg <henrik@malmberg.cc>
2025-08-14 13:06:08 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+// tailwind.config.js  (ESM)
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: { extend: {} },
   plugins: [],
-}
+};
+
